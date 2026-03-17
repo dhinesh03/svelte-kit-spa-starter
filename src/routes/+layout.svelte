69d598daa92d1a896feb6bb1089393a7dbb49724
dhinesh03 <dhinesh.gs@gmail.com>
2026-03-17 14:13:44 +0000

    feat: add ThemeSwitcher component to layout for improved theme management

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	import './layout.css';
+	import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte';
 	import { Toaster } from '$lib/components/ui/sonner';
 	import { ModeWatcher } from 'mode-watcher';
 
@@ -13,6 +14,9 @@
 
 <Toaster />
 <div class="flex h-svh flex-col">
+	<header class="flex items-center justify-end border-b px-4 py-2">
+		<ThemeSwitcher />
+	</header>
 	<main id="main" class="flex min-h-0 min-w-0 flex-1 flex-col overflow-hidden">
 		{@render children()}
 	</main>
